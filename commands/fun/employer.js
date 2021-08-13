fcfa548bccf260e9b394d1d4771a8e56036f1fba
Bridget Weston <jablweston@gmail.com>
2021-08-13 13:17:13 -0700

    stufg

--- commands/fun/employer.js
+++ commands/fun/employer.js
@@ -1,5 +1,5 @@
 // Employer Module
-//const categories = ["Animals", "Sports", "Service", "Academics", "Food", "Nature"]
+const categories = ["Animals", "Sports", "Service", "Academics", "Food", "Nature"]
 
 module.exports = {
     // Name of Command
@@ -13,8 +13,8 @@ module.exports = {
     // Execute Command - Parameters: message
     execute(message) {
         // Send Message
-        //const c = categories.join(', ') //@todo do not add comma to last element
+        const c = categories.join(', '); //@todo do not add comma to last element
         message.channel.send('Welcome employer! To add a volunteer position, please type !job [minimum age] [location] [hours] [category].');
-        //message.channel.send(`The categories are: ${c}`);
+        message.channel.send(`The categories are: ${c}`);
     },
 };
