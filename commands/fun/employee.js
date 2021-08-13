1e06d533282ba4be2326dcef78da9a758d24bc60
Bridget Weston <jablweston@gmail.com>
2021-08-13 13:25:45 -0700

    stuff

--- commands/fun/employee.js
+++ commands/fun/employee.js
@@ -1,4 +1,33 @@
 // Employee Module
+const categories = ["Animals", "Sports", "Service", "Academics", "Food", "Nature"]
+
+const jobs = [
+    {
+        title: 'Animal Shelter Volunteer',
+        description: 'wevuewniuewbbewiubewubgwkbgjew gjewbgjewg',
+        hours: 10,
+        location: 'Victoria',
+        category: 'Animals',
+        minAge: 14,
+    },
+    {
+        title: 'Kids Soccer Coach Volunteer',
+        description: 'wevuewniuewbbewiubewubgwkbgjew gjewbgjewg',
+        hours: 15,
+        location: 'Richmond',
+        category: 'Sports',
+        minAge: 16,
+    },
+    {
+        title: 'Food Bank Holiday Volunteer',
+        description: 'wevuewniuewbbewiubewubgwkbgjew gjewbgjewg',
+        hours: 5,
+        location: 'Victoria',
+        category: 'Service',
+        minAge: 14,
+    },
+]
+
 module.exports = {
     // Name of Command
     name: 'employee',
@@ -11,6 +40,10 @@ module.exports = {
     // Execute Command - Parameters: message
     execute(message) {
         // Send Message
-        message.channel.send(`Welcome student! We currently haveavailable!`);
+        const c = categories.join(', ');
+
+        message.channel.send(`Welcome student! We currently ${jobs.length} jobs available!`);
+        message.channel.send('To find a job, please enter your top three interests in this format: !find [one two three]');
+        message.channel.send(`Categories: ${c}`);
     },
 };
--- commands/fun/employer.js
+++ commands/fun/employer.js
@@ -13,7 +13,7 @@ module.exports = {
     // Execute Command - Parameters: message
     execute(message) {
         // Send Message
-        const c = categories.join(', '); //@todo do not add comma to last element
+        const c = categories.join(', ');
         message.channel.send('Welcome employer! To add a volunteer position, please type !job [minimum age] [location] [hours] [category].');
         message.channel.send(`The categories are: ${c}`);
     },
