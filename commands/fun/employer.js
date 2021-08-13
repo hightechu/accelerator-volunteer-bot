45898cea02d4541d7e0b0c812affd10a3cbfba7c
Bridget Weston <jablweston@gmail.com>
2021-08-13 12:51:15 -0700

    leo-1 2

--- commands/fun/employer.js
+++ commands/fun/employer.js
@@ -13,7 +13,7 @@ module.exports = {
         // Send Message
         message.channel.send('Welcome employer! We currently have "x" employees, please state the minimum age, location and time. Please write the info in the stated order.');
             // Execute Command - Parameters: message
-    execute(message)
+    //execute(message)
         // Send Message
         message.channel.send('Now please tell us what type of volunteering you are offering by reacting with the emojis below. (animal care, supervising children, charity.)');
         // Add Reaction
